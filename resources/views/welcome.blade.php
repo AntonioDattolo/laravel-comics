563e30a2abe3353a585a498d161f8b9f2390aba3
AntonioDattolo <antonio19794.a@gmail.com>
2024-06-29 14:50:57 +0200

    M3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,38 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body class="antialiased">
-        <div style="width: 100%; height:100vh; filter:grayscale(0)">
-            <h1>FUNGE</h1>
-        </div>
+
+        <section class="container">
+
+            <h1 class="text-center my-5" style="font-size: 80px;">Laravel Comics</h1>
+                <table class="w-100 bg-transparent">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Comics</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Details</th>
+                        </tr>
+                    </thead>
+                    @foreach ($film as $i => $datas)
+                        <tbody>
+                          <tr>
+                            <th scope="row">#{{$i+1}}</th>
+                            <td class="w-25"><img src="{{$datas['thumb']}}" class="card-img-top object-fit-fill border rounded" alt="..." style="height: 100px; width :100px"></td>
+                            <td class="w-50">{{$datas['title']}}</td>
+                            <td>{{$datas['price']}}</td>
+                            <td>
+                                <form action="/details" method="GET">
+                                    <button type="submit" style="background-color: black; color: greenyellow" name="selected_details" value="<?= $i ?>">
+                                        More details
+                                    </button>
+                                </form>
+                            </td>
+                          </tr>
+                    @endforeach
+                        </tbody>
+                </table>
+        </section>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $data = config("data");
-    return view('welcome');
+    $data = [
+        "film" => config("data")
+];
+    return view('welcome',$data);
+});
+
+Route::get('/home', function () {
+    $data = [
+        "film" => config("data")
+];
+    return view('welcome',$data);
+});
+Route::get('/details', function () {
+    $data = [
+        "film" => config("data")
+];
+    return view('details',$data);
 });
